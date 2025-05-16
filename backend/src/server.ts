2e0090463a690cd97f2a27632a6b60ce85600bc4
Sean Ashton V. Regalado <seanashton.regalado-23@cpu.edu.ph>
2025-05-16 21:47:47 +0800

    clear the TaskData array before running tests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,8 @@ import getRoutes from "./routes/get-routes";
 import postRoutes from "./routes/post-routes";
 import patchRoutes from "./routes/put-routes";
 import deleteRoutes from "./routes/delete-routes";
+import { TaskData } from "./task-data";
+
 const app = express();
 const PORT = 3000;
 
@@ -14,6 +16,13 @@ app.use("/get", getRoutes);
 app.use("/post", postRoutes);
 app.use("/patch", patchRoutes);
 app.use("/delete", deleteRoutes);
+
+if (process.env.NODE_ENV === "test") {
+  app.post("/test/reset", (req, res) => {
+    TaskData.length = 0;
+    res.status(204).send();
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
